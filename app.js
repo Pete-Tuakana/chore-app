418ef96a3e340f6383f102ff361782e5063c6f22
Pete-Tuakana <tuakana80@gmail.com>
2026-02-25 09:44:42 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const STORAGE_KEY = "alexanderChoreData"; 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
